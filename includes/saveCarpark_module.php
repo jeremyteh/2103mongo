c415122e4352da0522dee66d0f442ed6555e7494
Shahrul Nizam <1602602@SIT.Singaporetech.edu.sg>
2017-11-25 21:59:18 +0800

    Added Save Restaurant and Carpark Module

--- includes/saveCarpark_module.php
+++ includes/saveCarpark_module.php
@@ -2,26 +2,27 @@
   <?php
   $userID = $_SESSION['ID'];
   if (isset($_POST['saveFood']) == 'save'.$carparkID){
-    $insert = "INSERT INTO favouriteCarpark(carparkId, userId, status)
-    VALUES  ($carparkID,$userID , '1')";
-    if ($conn->query($insert) === TRUE) {
-    } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+    bulk = new MongoDB\Driver\BulkWrite();
+
+    $bulk->insert(['carparkId'=>(string)$_GET['carparkId'], 'userID'=>(string)$_SESSION['ID'], 'status'=>'1']);
+
+    $result = $mongodbManager->executeBulkWrite('foodfinderapp.favouritecarpark', $bulk);
   }
 
-  $checkFav = "SELECT COUNT(*) FROM favouritecarpark WHERE carparkId = ".$carparkID." AND userId = ".$_SESSION['ID'];
+  // Mongo
+  $filter = ['userID'=>(string)$_SESSION['ID'], 'carparkId'=>(string)$_GET['foodestablishmentId']];
+  $query = new \MongoDB\Driver\Query($filter);
+  $rows = $mongodbManager->executeQuery('foodfinderapp.favouritecarpark', $query);
+  $userRecord = $rows->toArray();
 
-  $result = mysqli_query($conn, $checkFav) or die(mysqli_connect_error());
-  $row = mysqli_fetch_array($result);
-  if($row[0] == 0)  {
-  echo "<form method='post' action='carpark.php?carparkId=".$carparkID."' id='form' name='form'>"
-  . "<input type='hidden' name='saveFood' value='save".$carparkID."'>"
-  . "<button class='button button-red button-wide' id='btn-save'>Save</button>"
-  . "</form>";}
-
+  if(count($userRecord) > 0) {
+    echo "<span class='res-saved'><i class='fa fa-check' aria-hidden='true'></i> Added to favourites</span>";
+  }
   else{
-       echo "<span class='res-saved'><i class='fa fa-check' aria-hidden='true'></i> Added to favourites</span>";
-  }
+     echo "<form method='post' action='restaurant.php?foodestablishmentId=".$_GET['carparkId']."' id='form' name='form'>"
+     . "<input type='hidden' name='saveFood' value='save".$_GET['carparkId']."'>"
+     . "<button class='button button-red button-wide' id='btn-save'>Save</button>"
+     . "</form>";
+   }
   ?>
 </div>
--- includes/saveRestaurant_module.php
+++ includes/saveRestaurant_module.php
@@ -1,26 +1,29 @@
 <div class="res-right-mod-wrap" id="saveFav">
   <?php
-  $userID = $_SESSION['ID'];
-  if (isset($_POST['saveFood']) == 'save'.$foodID){
-    $insert = "INSERT INTO favouritefood(foodestablishmentid, userid, status)
-    VALUES  ($foodID,$userID , '1')";
-    if ($conn->query($insert) === TRUE) {
-    } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-  }
-  $checkFav = "SELECT COUNT(*) FROM favouritefood WHERE foodEstablishmentId = ".$foodID." AND userId = ".$_SESSION['ID'];
+  if (isset($_POST['saveFood']) == 'save'.$_GET['foodestablishmentId']){
 
-  $result = mysqli_query($conn, $checkFav) or die(mysqli_connect_error());
-  $row = mysqli_fetch_array($result);
-  if($row[0] == 0)  {
-  echo "<form method='post' action='restaurant.php?foodEstablishmentId=".$foodID."' id='form' name='form'>"
-  . "<input type='hidden' name='saveFood' value='save".$foodID."'>"
-  . "<button class='button button-red button-wide' id='btn-save'>Save</button>"
-  . "</form>";
+    $bulk = new MongoDB\Driver\BulkWrite();
+
+    $bulk->insert(['foodestablishmentId'=>(string)$_GET['foodestablishmentId'], 'userID'=>(string)$_SESSION['ID'], 'status'=>'1']);
+
+    $result = $mongodbManager->executeBulkWrite('foodfinderapp.favouritefood', $bulk);
+
+}
+
+  $filter = ['userID'=>(string)$_SESSION['ID'], 'foodestablishmentId'=>(string)$_GET['foodestablishmentId']];
+  $query = new \MongoDB\Driver\Query($filter);
+  $rows = $mongodbManager->executeQuery('foodfinderapp.favouritefood', $query);
+  $userRecord = $rows->toArray();
+
+  if(count($userRecord) > 0) {
+    echo "<span class='res-saved'><i class='fa fa-check' aria-hidden='true'></i> Added to favourites</span>";
   }
   else{
-      echo "<span class='res-saved'><i class='fa fa-check' aria-hidden='true'></i> Added to favourites</span>";
-  }
+     echo "<form method='post' action='restaurant.php?foodestablishmentId=".$_GET['foodestablishmentId']."' id='form' name='form'>"
+     . "<input type='hidden' name='saveFood' value='save".$_GET['foodestablishmentId']."'>"
+     . "<button class='button button-red button-wide' id='btn-save'>Save</button>"
+     . "</form>";
+   }
+
   ?>
 </div>
