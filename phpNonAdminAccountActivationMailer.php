7910045c4ca7060b487055fe5a310242eb7e30ec
jeremyteh <magnificentbluez@hotmail.com>
2017-11-28 00:08:37 +0800

    change email for php mailer

--- phpNonAdminAccountActivationMailer.php
+++ phpNonAdminAccountActivationMailer.php
@@ -6,25 +6,22 @@ $mail = new PHPMailer();
 $mail->IsSMTP(); // set mailer to use SMTP
 //$mail->SMTPDebug  = 0;
 //$mail->Debugoutput = 'html';
-$mail->From = "jeremyteh8@gmail.com";
+$mail->From = "Foodpark";
 $mail->FromName = "Foodpark";
 $mail->Host = "smtp.gmail.com"; // specif smtp server
 $mail->SMTPSecure= "ssl"; // Used instead of TLS when only POP mail is selected
 $mail->Port = 465; // 465 Used instead of 587 when only POP mail is selected
 $mail->SMTPAuth = true;
 
-//$mail->Username = "";
-//$mail->Password = ""; // SMTP password
-$mail->Username = "jeremyteh8@gmail.com"; // SMTP username
-$mail->Password = "jtys#2804"; // SMTP password
-$mail->setFrom("jeremyteh8@gmail.com");  //add sender email address.
-$mail->AddAddress($_POST["email"]);
+$mail->Username = "foodparkco@gmail.com"; // SMTP username
+$mail->Password = "foodpark123"; // SMTP password
+$mail->setFrom("foodparkco@gmail.com");  //add sender email address.
+$mail->AddAddress("$email");
 $mail->WordWrap = 50; // set word wrap
 
 
-
 $mail->IsHTML(true); // set email format to HTML
-$mail->Subject = 'Foodpark Email Verification';
+$mail->Subject = 'Foodpark Account Email Verification';
 
 $message = 'Dear '.$_POST["firstName"].',<br><br>
 
