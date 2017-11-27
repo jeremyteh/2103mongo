1a2ce58b4f653a919dc27f8ee81ab68b93c028d9
ctjsctjs <ctjsctjs@gmail.com>
2017-11-27 23:16:25 +0800

    Featured working

--- abc.php
+++ abc.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 include_once 'protected/databaseconnection.php';
 
@@ -9,23 +9,55 @@ $options = [];
 // for loop print everything
 /*
 $filter = [
-	'$or' => [
-		['area' => new MongoDB\BSON\Regex(".*marina.*","i")],
-		['development' => new MongoDB\BSON\Regex(".*marina.*","i")]
-	]
+'$or' => [
+['area' => new MongoDB\BSON\Regex(".*marina.*","i")],
+['development' => new MongoDB\BSON\Regex(".*marina.*","i")]
+]
 ];*/
 
 
 
-$filter = ['name' => new MongoDB\BSON\Regex(".*marina.*","i")];
+$filter = [];
+$query = new MongoDB\Driver\Query($filter);
+$searchFoodEstablishments = $mongodbManager->executeQuery('foodfinderapp.review', $query)->toArray();
+
+$foodArray = array();
+$topThree = array();
+foreach ($searchFoodEstablishments as $document) {
+  $floatRating = (float)$document->AvgRating;
+  $intId = (int)$document->foodEstablishmentId;
+
+  if (array_key_exists($intId, $foodArray))
+  {
+    $foodArray[$intId]['rating'] += $floatRating;
+    $foodArray[$intId]['count'] += 1;
+    $foodArray[$intId]['average'] = $foodArray[$intId]['rating']/$foodArray[$intId]['count'];
+  }
+  else
+  {
+    $foodEst = array($intId => array('id'=>$intId, 'rating'=>$floatRating, 'count'=>1, 'average'=>$floatRating));
+    $foodArray+=$foodEst;
+  }
+}
+
+usort($foodArray, function ($item1, $item2) {
+  if ($item1['average'] == $item2['average']) return 0;
+  return $item1['average'] > $item2['average'] ? -1 : 1;
+});
+
+
+$filter = ['$or'=> array(
+['foodEstablishmentId'=>(string)$foodArray[0]['id']],
+['foodEstablishmentId'=>(string)$foodArray[1]['id']],
+['foodEstablishmentId'=>(string)$foodArray[2]['id']]
+)];
 
 $query = new MongoDB\Driver\Query($filter);
-$searchFoodEstablishments = $mongodbManager->executeQuery('foodfinderapp.foodestablishment', $query)->toArray();
+$featuredArray = $mongodbManager->executeQuery('foodfinderapp.foodestablishment', $query)->toArray();
 
-echo count($searchFoodEstablishments)."<br>";
-
-foreach ($searchFoodEstablishments as $document) {
-    echo $document->name."<br>";
+$featured = array();
+foreach ($featuredArray as $featuredPlace) {
+  echo $featuredPlace->name . '<br>';
 }
 
 
@@ -33,7 +65,7 @@ foreach ($searchFoodEstablishments as $document) {
 //$rows = $mongodbManager->executeQuery('foodfinderapp.carpark', $query);
 /*
 foreach ($rows as $dc){
-	echo $dc->email;
+echo $dc->email;
 }
 */
 /*********************************/
@@ -45,10 +77,10 @@ $query = new \MongoDB\Driver\Query($filter);
 $rows = $mongodbManager->executeQuery('foodfinderapp.user', $query);
 
 if(current($rows->toArray())) {
-	// means it exist
+// means it exist
 }
 else {
-	//it does not exist
+//it does not exist
 }
 
 $query = new \MongoDB\Driver\Query([]);
@@ -81,9 +113,9 @@ $result = $mongodbManager->executeBulkWrite('foodfinderapp.user', $bulk);
 $c = current($rows->toArray());
 
 if($c = null) {
-	echo $c->email;
+echo $c->email;
 }else {
-	echo 'jhdsjdhf';
+echo 'jhdsjdhf';
 }
 */
 //echo $c->email;
@@ -102,8 +134,8 @@ if($c = null) {
 $array = array();
 
 foreach ($rows as $document) {
-	echo $document->email;
-  //array_push($array, $document);
+echo $document->email;
+//array_push($array, $document);
 }
 */
 // $email = 'email';
